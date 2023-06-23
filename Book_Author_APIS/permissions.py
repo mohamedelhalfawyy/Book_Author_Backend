93ec526d9e49dfd972a501e532c150a2e20ba450
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-06-23 20:09:06 +0300

    create Views file for apis endpoints with authentication.

---
+++ Book_Author_APIS/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+class AuthorPermission(BasePermission):
+    def has_permission(self, request, view):
+        # Allow all users to read (GET) requests
+        if request.method in ['GET']:
+            return True
+        # Check if the user is an author for other operations
+        return request.user.is_authenticated and request.user.author
+
+    def has_object_permission(self, request, view, obj):
+        # Allow authors to edit their own books and pages
+        return obj.author.user == request.user
