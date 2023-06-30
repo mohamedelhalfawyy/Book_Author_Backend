a9c10107ae3671df0da78414ed61d79ec023aaa3
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-06-30 16:37:03 +0300

    Adjust the permissions and user model & Add the get users view to get the users from database and add another api to get the authors from database and check if the user is an author lastly add a new api to get the pages of a specific book

--- Book_Author_APIS/views.py
+++ Book_Author_APIS/views.py
@@ -1,16 +1,43 @@
 from rest_framework import generics, permissions
-from .models import Book, Page, User
-from .serializers import BookSerializer, PageSerializer, UserSerializer
+from .models import Book, Page, User, Author
+from .serializers import BookSerializer, PageSerializer, UserSerializer, AuthorSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Allow GET, HEAD, and OPTIONS requests for all users
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Allow write permissions only if the user is the author of the book
-        return obj.book.author.user == request.user
+        # Check if the user is authenticated and has an associated Author object
+        if request.user.is_authenticated:
+            author = Author.objects.filter(user=request.user).first()
+            return author is not None
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Allow write permissions for updating page content
+        if request.user.is_authenticated and isinstance(obj, Page):
+            return obj.book.author.user == request.user
+
+        # Allow write permissions only if the user is authenticated and the author is associated
+        if request.user.is_authenticated and hasattr(obj, 'author'):
+            return obj.author.user == request.user
+
+        return False
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -19,10 +46,29 @@ class UserRegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
+class IsAuthorAPIView(APIView):
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def get(self, request):
+        # The permission class will handle the authorization check
+        # If the user is not an author, it will raise a permission denied exception
+        author = request.user.author
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BookPagesAPIView(generics.ListAPIView):
+    serializer_class = PageSerializer
+
+    def get_queryset(self):
+        book_id = self.kwargs['bookid']
+        return Page.objects.filter(book_id=book_id)
 
 
 class BookRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
--- Book_Author_Backend/urls.py
+++ Book_Author_Backend/urls.py
@@ -1,19 +1,3 @@
-"""Book_Author_Backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from Book_Author_APIS.views import (
@@ -21,15 +5,18 @@ from Book_Author_APIS.views import (
     BookRetrieveUpdateDeleteView,
     PageListCreateView,
     PageRetrieveUpdateDeleteView,
-    UserRegisterView,
+    UserRegisterView, UserListAPIView, BookPagesAPIView, IsAuthorAPIView,
 )
 
 urlpatterns = [
+    path('users/', UserListAPIView.as_view(), name='user-list'),
+    path('is_author/', IsAuthorAPIView.as_view(), name='is_author'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegisterView.as_view(), name='user_register'),
     path('books/', BookListCreateView.as_view(), name='book_list_create'),
     path('books/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book_retrieve_update_delete'),
+    path('books/<int:bookid>/pages/', BookPagesAPIView.as_view(), name='book_pages'),
     path('pages/', PageListCreateView.as_view(), name='page_list_create'),
     path('pages/<int:pk>/', PageRetrieveUpdateDeleteView.as_view(), name='page_retrieve_update_delete'),
 ]
